873477d205dd87b2bd608b91ee0457e00513a4b8
Ivan Slastikhin <stopgo89@gmail.com>
2020-07-01 14:13:21 +0300

    App start is added

--- src/ru/jhonsy/home/FileManager.java
+++ src/ru/jhonsy/home/FileManager.java
@@ -1,12 +1,21 @@
 package ru.jhonsy.home;
 
+import ru.jhonsy.home.ui.UI;
+
+import javax.swing.*;
+
 /**
  * @Author: Ivan Slastikhin
  */
 public class FileManager {
 
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new UI();
+            }
+        });
     }
 
 }
